720a306e1dfad43f2cfa9d722852af9d9a8d00b6
badwin <yangyudong@linkv.sg>
2023-10-17 15:10:22 +0800

    x

--- xNetFuture/ViewController.m
+++ xNetFuture/ViewController.m
@@ -94,7 +94,8 @@ NSString *const kYDApplicationVPNListKey = @"kYDApplicationVPNListKey";
     [super viewDidLoad];
     // Do view setup here.
     self.vpnTextField.delegate = self;
-    self.vpnTextField.stringValue = @"vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogIl9VU1/nvo7lm70iLA0KICAiYWRkIjogInNpbmdhcG9yZS5jb20iLA0KICAicG9ydCI6ICI0NDMiLA0KICAiaWQiOiAiNmRlZGRiN2YtZTU1Ny00MmRiLWJmYTAtY2Y0MGIzNmIyN2UyIiwNCiAgImFpZCI6ICIwIiwNCiAgInNjeSI6ICJhdXRvIiwNCiAgIm5ldCI6ICJ3cyIsDQogICJ0eXBlIjogIm5vbmUiLA0KICAiaG9zdCI6ICJkLmZyZWVoMS54eXoiLA0KICAicGF0aCI6ICIvZG9uZ3RhaXdhbmcuY29tIiwNCiAgInRscyI6ICJ0bHMiLA0KICAic25pIjogIiIsDQogICJhbHBuIjogIiINCn0=";
+    // vmess, vless, trojan, ss
+    self.vpnTextField.stringValue = @"";
     NSLog(@"%@", [ExtProtocolParser parseURI:self.vpnTextField.stringValue]);
     self.startConnectButton.wantsLayer = YES;
     self.startConnectButton.layer.backgroundColor = [NSColor colorWithRed:2/255.0 green:187/255.0 blue:0/255.0 alpha:1.0].CGColor;
